3aee2ee8e620c01224fb8a3d1c8d0646062d3d04
khemagarwal <khemagarwal1@gmail.com>
2024-02-11 02:17:52 -0800

    fix: content changed

--- components/EndofExperiment.tsx
+++ components/EndofExperiment.tsx
@@ -3,8 +3,16 @@ import React, { useState, useEffect } from 'react';
 const EndOfExperiment = () => {
 
     return (
-        <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.8)', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 9999 }}>
-            <p>This is the end of the social experiment</p>
+        <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.8)', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 9999, flexDirection:'column' }}>
+            <p>Thanks for playing (safely)!  Remember, clicking unknown links can be risky, even in experiments.
+
+
+
+!</p>
+<br/>
+<p>Check if your info's been exposed: [https://haveibeenpwned.com/] #BeCyberSmart</p>
+<br/>
+<p>More coming soon! Thanks again for participating</p>
         </div>
     );
 };
